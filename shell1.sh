c1545dbf887711aaa1e654a46ad09c2b3a1a0059
MehulCUB <mehulkumar.patel@colorado.edu>
2021-09-19 08:44:14 -0600

    removing buildroot

---
+++ shell1.sh
@@ -0,0 +1,20 @@
+which which
+which sed
+which make 
+which binutils
+which build-essential
+which gcc 
+which g++ 
+which bash
+which patch
+which gzip
+which bzip2
+which perl 
+which tar
+which cpio
+which unzip
+which rsync
+which file
+which bc
+
+which wget
