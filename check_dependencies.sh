242498ee0e53d317f9891600e4bd2b4f4a72e219
MehulCUB <mehulkumar.patel@colorado.edu>
2021-09-20 12:17:55 -0600

    pushing from second local

---
+++ check_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#script for checking if dependencies/ packages installed or not
+which which
+which sed
+which make 
+which binutils
+which build-essential
+which gcc 
+which g++ 
+which bash
+which patch
+which gzip
+which bzip2
+which perl 
+which tar
+which cpio
+which unzip
+which rsync
+which file
+which bc
+
+which wget
